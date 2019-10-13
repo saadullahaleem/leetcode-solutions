6ee21c6b9fb0ac6d0375cf564a8534f4a38919ae
Saadullah <aleemsaadullah@gmail.com>
2019-10-14 03:42:22 +0400

    minor changes to longest subsrting solution

--- 3-longest-substring.py
+++ 3-longest-substring.py
@@ -3,9 +3,9 @@ from collections import deque
 
 class Solution:
     def lengthOfLongestSubstring(self, string: str) -> int:
-        occurred = dict()
+        occurred = {}
         window_left = window_right = 0
-        max = 0
+        maximum = 0
 
         for i, ch in enumerate(string):
             if ch not in occurred:
@@ -14,8 +14,9 @@ class Solution:
                 while window_left <= occurred[ch]:
                     window_left += 1
                 window_right = i + 1
+
             occurred[ch] = i
-            max = window_right - window_left if (window_right - window_left) > max else max
+            current_maximum = window_right - window_left
+            maximum = current_maximum if current_maximum > maximum else maximum
 
-        return max
-
+        return maximum
